1577d7049f023e4e05ce1018e929e4271bbc3f8c
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2020-11-02 11:40:05 +0200

    12

--- src/components/register.js
+++ src/components/register.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class register extends Component {
-    render() {
-        return (
-            <div>
-                <h1>1</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>1</h1>
+      </div>
+    );
+  }
 }
 
-export default register
+export default register;

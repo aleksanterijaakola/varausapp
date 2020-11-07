fce921e8a1ff22e75214ac4664a57e76f344e55a
AnneRei <anne.reichert97@gmail.com>
2020-11-07 14:12:13 +0200

    Implement navigation components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import Login from "./components/Login";
+import { Router } from 'react-router-dom';
+import history from './services/history';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
 export default App;
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 import '../App.css';
-import App from '../App';
-import reportWebVitals from '../reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      {/* alter Route component to use variable "isPrivate and */}
+      {/* redirect user to Login page if route does not exist or user is not authenticated */}
+      <Route component={Login} />
+    </Switch>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

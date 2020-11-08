5d6ccfb28a6447e557d44f2caf2fbbd6c36427e7
Janaina Kaufmann <janainaflor.kaufmann@edu.turkuamk.fi>
2020-11-08 23:10:32 +0200

    Some Design implemented not done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import '../App.css';
 
+import thefirmalogo from '../img/thefirma_white.png'
+import turkuamklogo from '../img/turku_amk.png'
+
 
 const Login = () => {
   const [password, setPassword] = useState("");
@@ -20,13 +23,15 @@ const Login = () => {
   return (
     <>
       <article className="login">
+        <img src={thefirmalogo} alt="thefirmalogo" width="310px"></img>
+        <h1>Booking Computer App</h1>
         <form className="form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
           <div className="form-control">
-            <h1>Sign In</h1>
             <input
               type="text"
               id="email"
-              placeholder="Email"
+              placeholder="E-MAIL"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -36,23 +41,29 @@ const Login = () => {
             <input
               type="password"
               id="password"
-              placeholder="Password"
+              placeholder="PASSWORD"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
               validate
             />
           </div>
-          <button type="submit" className="buttonContainer">
-            Login
+          <div className="button-container">
+            <button type="submit">
+              LOGIN
           </button>
-          <button type="submit" className="buttonContainer">
-            Register
+            <button type="submit">
+              REGISTER
           </button>
-          <button type="submit" className="buttonContainer">
-            Forgot password?
+          <div className="button-container-forgot">
+          <button type="submit" className="forgot-button">
+              FORGOT PASSWORD ?
           </button>
+          </div>
+
+          </div>
         </form>
+        <img src={turkuamklogo} alt="turkuamklogo" width="200px"></img>
       </article>
     </>
   );

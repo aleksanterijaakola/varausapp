81a2001751699de96e4efedbf87c6813b6da11bd
Aleksanteri Jaakola <aleksanteri.jaakola@edu.turkuamk.fi>
2020-11-13 17:31:29 +0200

    Revert "Merge branch 'dev_janaina_design' into 'master'"
    This reverts merge request !9

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import ReactFormInputValidation from "react-form-input-validation";
 import "../App.css";
 
-import thefirmalogo from '../img/thefirma_white.png'
-import turkuamklogo from '../img/turku_amk.png'
-
 class Register extends Component {
   constructor(props) {
     super(props);
@@ -30,48 +27,47 @@ class Register extends Component {
   render() {
     return (
       <React.Fragment>
-        <article class="text-center">
-          <form class="form-signin" onSubmit={this.form.handleSubmit}>
-            <img src={thefirmalogo} alt="thefirmalogo" className="mb-1" height="70px" />
-            <h1 class="h4 mb-5 font-weight-normal text-white">Booking Computer App</h1>
-            <h2 class="h5 mb-3 font-weight-normal text-white text-left">Register</h2>
-            <label for="inputMail" class="sr-only">Email Address</label>
-            <input
-              type="email"
-              value={this.state.fields.email}
-              onBlur={this.form.handleBlurEvent}
-              onChange={this.form.handleChangeEvent}
-              name="email"
-              placeholder="E-MAIL"
-              class="form-control text-white"
-            />
+        <h1>Registration</h1>
+        <form onSubmit={this.form.handleSubmit}>
+          <p>
+            <label>
+              <input
+                type="email"
+                value={this.state.fields.email}
+                onBlur={this.form.handleBlurEvent}
+                onChange={this.form.handleChangeEvent}
+                name="email"
+                placeholder="Email"
+              />
+            </label>
             <label className="error">
               {this.state.errors.email ? this.state.errors.email : ""}
             </label>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input
-              type="password"
-              value={this.state.fields.password}
-              onChange={this.form.handleChangeEvent}
-              onBlur={this.form.handleBlurEvent}
-              name="password"
-              placeholder="PASSWORD"
-              class="form-control text-white"
-            />
+          </p>
+
+          <p>
+            <label>
+              <input
+                type="password"
+                value={this.state.fields.password}
+                onChange={this.form.handleChangeEvent}
+                onBlur={this.form.handleBlurEvent}
+                name="password"
+                placeholder={"password"}
+              />
+            </label>
             <label className="error">
               {this.state.errors.password ? this.state.errors.password : ""}
             </label>
-            <div class="d-flex justify-content-around mt-3 mb-4">
-              <button onClick={() => this.props.history.push("/login")} class="btn btn-primary text-white">
-                LOGIN
-              </button>
-              <button type="Submit" class="btn btn-primary text-white">
-                SIGN UP
-              </button>
-            </div>
-            <img src={turkuamklogo} alt="turkuamklogo" class="mt-5 mb-4" height="70px" />
-          </form>
-        </article>
+          </p>
+
+          <p>
+            <button type="Submit">Sign Up!</button>
+          </p>
+        </form>
+        <button onClick={() => this.props.history.push("/login")}>
+          Back to Login
+        </button>
       </React.Fragment>
     );
   }

1d99cad0329a54b64f788b7ec3a4a5147b975b26
AnneRei <anne.reichert97@gmail.com>
2020-11-16 19:27:13 +0200

    Added repeat password with equality validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,27 +1,35 @@
 import React, { useRef } from 'react'
-import ReactFormInputValidation from 'react-form-input-validation'
 import '../App.css'
 import thefirmalogo from '../img/thefirma_white.png'
 import turkuamklogo from '../img/turku_amk.png'
 import { useHistory } from 'react-router-dom'
 
+
 const Register = () => {
   const history = useHistory()
-  const error = useRef()
-  const email = useRef()
-  const password = useRef()
+  const email = useRef("")
+  const repeatEmail = useRef("")
+  const password = useRef("")
 
-  async function handleSubmit (e) {
-    e.preventDefault()
-    console.log(e.email)
+  async function onSubmit (e) {
     console.log(email.current.value)
-    history.push('/dashboard')
+    if(!repeatEmailValidation(repeatEmail.current.value)){
+      alert("Email does not match")
+    }
+    else {
+      history.push('/dashboard')
+    }
   }
+
+
+  const repeatEmailValidation = value => (
+    value === email.current.value
+  )
 
   return (
     <React.Fragment>
       <article class="text-center">
-        <form class="form-signin" onSubmit={handleSubmit}>
+        <form class="form-signin"  onSubmit={onSubmit}>
           <img src={thefirmalogo} alt="thefirmalogo" className="mb-1" height="70px"/>
           <h1 class="h4 mb-5 font-weight-normal text-white">
             Booking Computer App
@@ -35,32 +43,41 @@ const Register = () => {
           <input
             type="email"
             ref={email}
-            //onBlur={this.form.handleBlurEvent}
-            //onChange={this.form.handleChangeEvent}
+            required
             name="email"
             placeholder="E-MAIL"
             class="form-control"
           />
+          <label htmlFor="inputMail" className="sr-only">
+            Email Address
+          </label>
+          <input
+            type="email"
+            ref={repeatEmail}
+            required
+            name="repeatEmail"
+            placeholder="REPEAT E-MAIL"
+            className="form-control"
 
+          />
           <label for="inputPassword" class="sr-only">
             Password
           </label>
           <input
             type="password"
-            ref={password}
-            //onChange={this.form.handleChangeEvent}
-            //onBlur={this.form.handleBlurEvent}
             name="password"
+            ref={password}
             placeholder="PASSWORD"
+            required
             class="form-control"
           />
-
           <div class="d-flex justify-content-around mt-3 mb-4">
             <button
               onClick={() => history.push('/login')}
               class="btn btn-primary text-white"
             >
               LOGIN
+
             </button>
             <button type="Submit" class="btn btn-primary text-white">
               SIGN UP
@@ -68,6 +85,7 @@ const Register = () => {
           </div>
           <img src={turkuamklogo} alt="turkuamklogo" class="mt-5 mb-4" height="70px"/>
         </form>
+
       </article>
     </React.Fragment>
   )

31c06d1e9e7d843789b2d5c792c8d39920cccb4c
Aleksanterijaakola <aleksanterijaakola@icloud.com>
2020-11-17 15:24:18 +0200

    Firebase authentication for registering

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,36 +1,51 @@
-import React, { useRef } from 'react'
-import '../App.css'
-import thefirmalogo from '../img/thefirma_white.png'
-import turkuamklogo from '../img/turku_amk.png'
-import { useHistory } from 'react-router-dom'
-
+import React, { useRef, useState } from "react";
+import "../App.css";
+import { useAuth } from "../contexts/AuthContext";
+import thefirmalogo from "../img/thefirma_white.png";
+import turkuamklogo from "../img/turku_amk.png";
+import { useHistory, Link } from "react-router-dom";
 
 const Register = () => {
-  const history = useHistory()
-  const email = useRef("")
-  const repeatEmail = useRef("")
-  const password = useRef("")
+  const { signup } = useAuth();
+  const history = useHistory();
+  const email = useRef();
+  const repeatEmail = useRef();
+  const password = useRef();
+  const [error, setError] = useState("");
 
-  async function onSubmit (e) {
-    console.log(email.current.value)
-    if(!repeatEmailValidation(repeatEmail.current.value)){
-      alert("Email does not match")
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(email.current.value)
+    /*if(!repeatEmailValidation(repeatEmail.current.value)){
+      alert("Email does not match") */
+    if (email.current.value !== repeatEmail.current.value) {
+      return setError("Emails do not match");
     }
-    else {
-      history.push('/dashboard')
+
+    try {
+      setError("");
+      await signup(email.current.value, password.current.value);
+      history.push("/dashboard");
+    } catch {
+      setError("Password needs to be at least 6 characters long");
     }
   }
 
-
-  const repeatEmailValidation = value => (
+  /*const repeatEmailValidation = value => (
     value === email.current.value
-  )
+  ) */
 
   return (
     <React.Fragment>
       <article class="text-center">
-        <form class="form-signin"  onSubmit={onSubmit}>
-          <img src={thefirmalogo} alt="thefirmalogo" className="mb-1" height="70px"/>
+        {error && <alert>{error}</alert>}
+        <form class="form-signin" onSubmit={handleSubmit}>
+          <img
+            src={thefirmalogo}
+            alt="thefirmalogo"
+            className="mb-1"
+            height="70px"
+          />
           <h1 class="h4 mb-5 font-weight-normal text-white">
             Booking Computer App
           </h1>
@@ -58,7 +73,6 @@ const Register = () => {
             name="repeatEmail"
             placeholder="REPEAT E-MAIL"
             className="form-control"
-
           />
           <label for="inputPassword" class="sr-only">
             Password
@@ -73,22 +87,23 @@ const Register = () => {
           />
           <div class="d-flex justify-content-around mt-3 mb-4">
             <button
-              onClick={() => history.push('/login')}
+              onClick={() => history.push("/login")}
               class="btn btn-primary text-white"
             >
               LOGIN
-
             </button>
-            <button type="Submit" class="btn btn-primary text-white">
-              SIGN UP
-            </button>
+            <button class="btn btn-primary text-white">SIGN UP</button>
           </div>
-          <img src={turkuamklogo} alt="turkuamklogo" class="mt-5 mb-4" height="70px"/>
+          <img
+            src={turkuamklogo}
+            alt="turkuamklogo"
+            class="mt-5 mb-4"
+            height="70px"
+          />
         </form>
-
       </article>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;

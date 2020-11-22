8e1de500c9b231a3d012cad6b9911114d474e015
AnneRei <anne.reichert97@gmail.com>
2020-11-22 14:57:29 +0200

    Include Booking button, computer selection and prototype time selections

--- src/pages/BookingInterface.js
+++ src/pages/BookingInterface.js
@@ -1,17 +1,25 @@
-import React, { useState, useRef } from "react";
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
-import { BiCalendar } from "react-icons/bi";
-
+import React, { useState} from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import { BiCalendar } from 'react-icons/bi'
 
 const BookingInterface = () => {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date())
+  const [selectedTimes, setSelectedTimes] = useState([])
+  const [selectedComputer, setSelectedComputer] = useState([])
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    console.log(date)
+  const handleSelectTimes = function (selectedItems) {
+    const times = []
+    for (let i = 0; i < selectedItems.length; i++) {
+      times.push(selectedItems[i].value)
+    }
+    setSelectedTimes(times)
   }
 
+  async function handleSubmit (e) {
+    e.preventDefault()
+    console.log(date, selectedTimes, selectedComputer)
+  }
 
   return (
     <div class="book-page">
@@ -22,26 +30,39 @@ const BookingInterface = () => {
             <div class="mb-4">
               <label class="sr-only">Date</label>
               <label>
-              <DatePicker
-                required
-                format={"y-MM-dd"}
-                selected={date}
-                onChange={setDate}
-                value={date}
-              />
+                <DatePicker
+                  required
+                  format={'y-MM-dd'}
+                  selected={date}
+                  onChange={setDate}
+                  value={date}
+                />
                 <BiCalendar/>
               </label>
             </div>
             <div>
               <label class="sr-only">Computer ID</label>
-              <select class="form-control">
-                <option>Computer 1</option>
-                <option>Computer 2</option>
-                <option>Computer 3</option>
-                <option>Computer 4</option>
-                <option>Computer 5</option>
+              <select class="form-control" onChange={(e) => {setSelectedComputer(e.target.selectedOptions[0].value)}}>
+                <option value="1">Computer 1</option>
+                <option value="2">Computer 2</option>
+                <option value="3">Computer 3</option>
+                <option value="4">Computer 4</option>
+                <option value="5">Computer 5</option>
               </select>
             </div>
+          </div>
+          <div className="timeContainer">
+            <select multiple={true} value={selectedTimes} onChange={(e) => {handleSelectTimes(e.target.selectedOptions)}}>
+              <option value="12:00">12:00</option>
+              <option value="13:00">13:00</option>
+              <option value="14:00">14:00</option>
+              <option value="15:00">15:00</option>
+            </select>
+          </div>
+          <div className="d-flex justify-content-around mt-3 mb-4">
+            <button type="submit" className="btn btn-primary text-white">
+              BOOK SELECTION
+            </button>
           </div>
         </form>
       </article>
@@ -50,7 +71,7 @@ const BookingInterface = () => {
       </h6>
     </div>
   )
-};
+}
 
-export default BookingInterface;
+export default BookingInterface
 

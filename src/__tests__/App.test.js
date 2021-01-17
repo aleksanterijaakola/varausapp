40aeb137516bf490c827bd237817c4c6d0739317
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-01-17 17:59:38 +0200

    added another test case

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Button from '../pages/Login'
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import Button from "../pages/Login";
+import App from "../pages/App";
 
-
-describe('Button', () => {
- it('should be defined', () => {
-   expect(Button).toBeDefined();
- });
- it('should render correctly', () => {
-   const tree = shallow(
-     <Button name='button test' />
-   );
-   expect(tree).toMatchSnapshot();
- });
+describe("Button", () => {
+  it("should be defined", () => {
+    expect(Button).toBeDefined();
+  });
+  it("should render correctly", () => {
+    const tree = shallow(<Button name="button test" />);
+    expect(tree).toMatchSnapshot();
+  });
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  });
 });

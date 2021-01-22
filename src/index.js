21da16d6b49aa92e322ee50148e47e5e9d80d1a8
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-01-22 09:24:35 +0200

    restructured files and removed some

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-import App from "./pages/App";
+import App from './components/App'
 import "bootstrap/dist/css/bootstrap.css";
 import * as serviceWorkerRegistration from "./Register-serviceworker";
 

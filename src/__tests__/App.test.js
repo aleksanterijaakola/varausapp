ed79526dbd90617e965d477e8dbec3a589171e90
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-01-22 09:30:07 +0200

    corrected imports for tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
-import Button from "../pages/Login";
-import App from "../pages/App";
+import Button from "../components/Login";
+import App from "../components/App";
 
 describe("Button", () => {
   it("should be defined", () => {

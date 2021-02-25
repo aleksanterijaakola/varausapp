21209fff77ebc48d54845816b9a4612572286f24
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-02-25 17:07:45 +0200

    123

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,48 @@ exports.randomNumber = functions.https.onRequest((req, res) => {
   const number = Math.round(Math.random() * 100);
   res.send(number.toString());
 });
+
+
+exports.isAdmin = functions.https.onCall((data) => {
+  return auth()
+    .getUserByEmail(data.email)
+    .then((userRecord) => {
+      return userRecord.customClaims.admin;
+    })
+    .catch((err) => {
+      return err;
+    });
+});
+
+
+exports.deleteUser = https.onCall((data) => {
+  return auth()
+    .getUserByEmail(data.email)
+    .then((user) => {
+      return auth().deleteUser(user.uid).then;
+    })
+    .then(() => {
+      return "Successfully deleted user " + data.email;
+    })
+    .catch((err) => {
+      return err.message;
+    });
+});
+
+
+
+exports.listAllUser = https.onCall((data) => {
+  return auth()
+    .listUsers(1000)
+    .then((listUserResult) => {
+      listUserResult.users.forEach((userRecord) => {
+        console.log("user", userRecord.email);
+        data.list.push(userRecord.email);
+      });
+      return data.list;
+    })
+    .catch((error) => {
+      console.log("Error listing useres: ", error);
+    });
+});
+

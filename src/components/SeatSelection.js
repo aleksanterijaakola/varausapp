57748a56fc3850fea376bc507ed06c60cfa73e35
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-03-16 12:42:46 +0200

    refactoring

--- src/components/SeatSelection.js
+++ src/components/SeatSelection.js
@@ -1,30 +1,21 @@
-import React, { Component } from "react";
+import React, {useState} from 'react';
 import ReactTooltip from "react-tooltip";
 import SeatPicker from "react-seat-picker";
 import "../App.css";
 
-export default class App extends Component {
-  state = {
-    loading: false,
-  };
-  /* * TODO: 
-Convert all to functional components
-// Max selected is now 3, make it so you can only choose 1
-Remove tooltip for seat.
-Remove 'A,B,C,D' tooltip. 
 
 
-*/
-  addSeatCallbackContinousCase = (
+function SeatSelection() {
+  const [loading, setLoading] = useState(false)
+
+  const AddSeatCallbackContinousCase = (
     { row, number, id },
     addCb,
     params,
     removeCb
   ) => {
     this.setState(
-      {
-        loading: false,
-      },
+     
       async () => {
         if (removeCb) {
           await new Promise((resolve) => setTimeout(resolve, 750));
@@ -42,7 +33,7 @@ Remove 'A,B,C,D' tooltip.
     );
   };
 
-  removeSeatCallback = ({ row, number, id }, removeCb) => {
+  const removeSeatCallback = ({ row, number, id }, removeCb) => {
     this.setState(
       {
         loading: false,
@@ -58,82 +49,80 @@ Remove 'A,B,C,D' tooltip.
     );
   };
 
-  render() {
-    const rows = [
-      [
-        { id: 1, number: 1, isSelected: true, tooltip: "Reserved by you" },
-        { id: 2, number: 2},
-        null,
-        {
-          id: 3,
-          number: "3",
-          isReserved: true,
-          orientation: "east",
-          tooltip: "Reserved by Rogger",
-        },
-        { id: 4, number: "4", orientation: "west" },
-        null,
-        { id: 5, number: 5 },
-        { id: 6, number: 6 },
-      ],
-      [
-        {
-          id: 7,
-          number: 1,
-          isReserved: true,
-          tooltip: "Reserved by Matthias Nadler",
-        },
-        { id: 8, number: 2, isReserved: true },
-        null,
-        { id: 9, number: "3", isReserved: true, orientation: "east" },
-        { id: 10, number: "4", orientation: "west" },
-        null,
-        { id: 11, number: 5 },
-        { id: 12, number: 6 },
-      ],
-      [
-        { id: 13, number: 1 },
-        { id: 14, number: 2 },
-        null,
-        { id: 15, number: 3, isReserved: true, orientation: "east" },
-        { id: 16, number: "4", orientation: "west" },
-        null,
-        { id: 17, number: 5 },
-        { id: 18, number: 6 },
-      ],
-      [
-        { id: 19, number: 1, tooltip: "Cost: 25$" },
-        { id: 20, number: 2 },
-        null,
-        { id: 21, number: 3, orientation: "east" },
-        { id: 22, number: "4", orientation: "west" },
-        null,
-        { id: 23, number: 5 },
-        { id: 24, number: 6 },
-      ],
-      [
-        { id: 25, number: 1, isReserved: true },
-        { id: 26, number: 2, orientation: "east" },
-        null,
-        { id: 27, number: "3", isReserved: true },
-        { id: 28, number: "4", orientation: "west" },
-        null,
-        { id: 29, number: 5, tooltip: "Cost: 11$" },
-        { id: 30, number: 6, isReserved: true },
-      ],
-    ];
-    const { loading } = this.state;
-    return (
-      <div>
+  const rows = [
+    [
+      { id: 1, number: 1, isSelected: true, tooltip: "Reserved by you" },
+      { id: 2, number: 2},
+      null,
+      {
+        id: 3,
+        number: "3",
+        isReserved: true,
+        orientation: "east",
+        tooltip: "Reserved by Rogger",
+      },
+      { id: 4, number: "4", orientation: "west" },
+      null,
+      { id: 5, number: 5 },
+      { id: 6, number: 6 },
+    ],
+    [
+      {
+        id: 7,
+        number: 1,
+        isReserved: true,
+        tooltip: "Reserved by Matthias Nadler",
+      },
+      { id: 8, number: 2, isReserved: true },
+      null,
+      { id: 9, number: "3", isReserved: true, orientation: "east" },
+      { id: 10, number: "4", orientation: "west" },
+      null,
+      { id: 11, number: 5 },
+      { id: 12, number: 6 },
+    ],
+    [
+      { id: 13, number: 1 },
+      { id: 14, number: 2 },
+      null,
+      { id: 15, number: 3, isReserved: true, orientation: "east" },
+      { id: 16, number: "4", orientation: "west" },
+      null,
+      { id: 17, number: 5 },
+      { id: 18, number: 6 },
+    ],
+    [
+      { id: 19, number: 1, tooltip: "Cost: 25$" },
+      { id: 20, number: 2 },
+      null,
+      { id: 21, number: 3, orientation: "east" },
+      { id: 22, number: "4", orientation: "west" },
+      null,
+      { id: 23, number: 5 },
+      { id: 24, number: 6 },
+    ],
+    [
+      { id: 25, number: 1, isReserved: true },
+      { id: 26, number: 2, orientation: "east" },
+      null,
+      { id: 27, number: "3", isReserved: true },
+      { id: 28, number: "4", orientation: "west" },
+      null,
+      { id: 29, number: 5, tooltip: "Cost: 11$" },
+      { id: 30, number: 6, isReserved: true },
+    ],
+  ];
+  
+  return (
+    <div>
         <div></div>
         <h1>Seat Picker</h1>
-        <h2>Selected {this.props.addSeatCallbackContinousCase}</h2>
         <div style={{ marginTop: "100px", position: "relative", left: "35px" }}>
           <SeatPicker
-            addSeatCallback={this.addSeatCallbackContinousCase}
-            removeSeatCallback={this.removeSeatCallback}
-            rows={rows}
+            addSeatCallback={AddSeatCallbackContinousCase}
+            removeSeatCallback={removeSeatCallback}
             maxReservableSeats={1}
+            rows={rows}
             alpha
             visible
             selectedByDefault
@@ -143,6 +132,7 @@ Remove 'A,B,C,D' tooltip.
           />
         </div>
       </div>
-    );
-  }
+  )
 }
+
+export default SeatSelection

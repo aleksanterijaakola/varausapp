1caebc8e01a3451efb1200ac762787718f7ecf15
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-04-07 11:13:43 +0300

    Tailwind configuration, rewrote login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
-import "../App.css";
-import { Alert } from "react-bootstrap";
-import thefirmalogo from "../Assets/img/turkuamk.png"
-import turkuamklogo from "../Assets/img/thefirma.png"
+//import "../App.css";
+//import { Alert } from "react-bootstrap";
+import thefirmalogo from "../Assets/img/turkuamk.png";
+import turkuamklogo from "../Assets/img/thefirma.png";
 import { auth } from "../firebase";
-
+import "../styles/tailwind.css";
 import { AuthContext } from "../contexts/AuthContext";
 
 const Login = () => {
@@ -29,85 +29,65 @@ const Login = () => {
   };
 
   return (
-    <article className="text-center">
-      {/**/}
-      {error && (
-        <Alert className="login-alert" variant="danger">
-          {alert}
-        </Alert>
-      )}
-      <form className="form-signin" onSubmit={signIn}>
-        <img
-          src={thefirmalogo}
-          alt="Responsive image"
-          className="mb-1"
-          height="70px"
-        />
-        <h1 className="h4 mb-5 font-weight-normal text-white">
-          Booking Computer App
-        </h1>
-        <h2 className="h5 mb-3 font-weight-normal text-white text-left">
-          Login
-        </h2>
-        <label htmlFor="inputMail" className="sr-only">
-          Email Address
-        </label>
-        <input
-          type="text"
-          id="email"
-          placeholder="E-MAIL"
-          required
-          value={email}
-          className="form-control shadow-none"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="inputPassword" className="sr-only">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          placeholder="PASSWORD"
-          required
-          value={password}
-          className="form-control mt-3  shadow-none"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className="d-flex justify-content-around mt-3 mb-4">
-          <button
-            type="submit"
-            className="LoginRegisterForgotButton"
-            style={{ marginRight: "2px" }}
-          >
-            LOGIN
-          </button>
-          <button
-            className="LoginRegisterForgotButton"
-            onClick={() => history.push("/register")}
-            style={{ backgroundColor: "#008CBA" }}
-          >
-            REGISTER
-          </button>
-        </div>
-        <div>
-          <button
-            type="button"
-            className="LoginRegisterForgotButton"
-            onClick={() => history.push("/forgotpassword")}
-            style={{ width: "300px" }}
-          >
-            FORGOT PASSWORD
-          </button>
-        </div>
-
-        <img
-          src={turkuamklogo}
-          alt="turkuamklogo"
-          className="mt-5 mb-4"
-          height="70px"
-        />
-      </form>
-    </article>
+    <>
+      <div class="w-full max-w-xs relative object-center">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={signIn}>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="Username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={() => history.push("/dashboard")}
+            >
+              Sign In
+            </button>
+            <a
+              class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="/forgotpassword"
+              
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p class="text-center text-gray-500 text-xs">
+          &copy;2021 theFIRMA. All rights reserved.
+        </p>
+      </div>
+    </>
   );
 };
 

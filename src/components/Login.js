cef0b3b08a400936a599554642191a2c0cc35d34
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-04-07 11:52:34 +0300

    123

--- src/components/Login.js
+++ src/components/Login.js
@@ -43,6 +43,7 @@ const Login = () => {
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="email"
               type="text"
+              required
               placeholder="Username"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -69,17 +70,10 @@ const Login = () => {
           <div class="flex items-center justify-between">
             <button
               class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
               onClick={() => history.push("/dashboard")}
             >
               Sign In
-            </button>
-            <button
-              class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-              type="button"
-              onClick={() => history.push("/dashboard")}
-            >
-              Register
             </button>
             <a
               class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"

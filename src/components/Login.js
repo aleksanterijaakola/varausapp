71d2f5eb4e3b81a40606b81746364a6958972f2c
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-04-08 10:31:15 +0300

    Tailwind CSS for login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
-//import "../App.css";
-//import { Alert } from "react-bootstrap";
 import thefirmalogo from "../Assets/img/turkuamk.png";
 import turkuamklogo from "../Assets/img/thefirma.png";
 import { auth } from "../firebase";
@@ -12,8 +10,8 @@ const Login = () => {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error] = useState("");
-  // TODO: error message implementation unfinished
+  const [error] = useState(""); 
+
 
   const { currentUser } = useContext(AuthContext);
 
@@ -30,8 +28,14 @@ const Login = () => {
 
   return (
     <>
-      <div class="w-full max-w-xs container relative top-40">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={signIn}>
+      <div class="w-6/12 container relative top-20" >
+        <img class="" src={turkuamklogo} alt=""></img>
+      </div>
+      <div class="w-full max-w-xs container relative top-28">
+        <form
+          class="bg-white bg-opacity-30 shadow-sm rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={signIn}
+        >
           <div class="mb-4">
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
@@ -40,11 +44,11 @@ const Login = () => {
               Username
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none "
               id="email"
               type="text"
               required
-              placeholder="Username"
+              placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             ></input>
@@ -71,14 +75,12 @@ const Login = () => {
             <button
               class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
-              onClick={() => history.push("/dashboard")}
             >
               Sign In
             </button>
             <a
               class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
               href="/forgotpassword"
-              
             >
               Forgot Password?
             </a>
@@ -87,6 +89,9 @@ const Login = () => {
         <p class="text-center text-gray-500 text-xs">
           &copy;2021 theFIRMA. All rights reserved.
         </p>
+      </div>
+      <div class="w-6/12 container relative top-40">
+        <img src={thefirmalogo} alt=""></img>
       </div>
     </>
   );

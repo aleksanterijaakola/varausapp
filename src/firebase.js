26449b6f6e9de0b5cbfb3123083494f392535350
aleksanterijaakola <44531766+aleksanterijaakola@users.noreply.github.com>
2021-05-14 11:53:50 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAmapZ4HyS11Vh0elY2PWz6_eyHd-wcr5Y",
-  authDomain: "booking-app-48201.firebaseapp.com",
-  databaseURL: "https://booking-app-48201.firebaseio.com",
-  projectId: "booking-app-48201",
-  storageBucket: "booking-app-48201.appspot.com",
-  messagingSenderId: "362177666940",
-  appId: "1:362177666940:web:757c50d77d2f5c5cf60ef9"
+  apiKey: "*********************************",
+  authDomain: "*********************************",
+  databaseURL: "*********************************",
+  projectId: "*********************************",
+  storageBucket: "*********************************",
+  messagingSenderId: "*********************************",
+  appId: "*********************************"
 });
 
 
